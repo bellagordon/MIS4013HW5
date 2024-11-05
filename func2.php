0469a85904da51011f2dca3d7ae0c14ff48bfcee
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-04 19:41:12 -0600

    Update func2.php

--- func2.php
+++ func2.php
@@ -5,30 +5,33 @@
 </head>
 <body>
     <h1>Function 1</h1>
-<div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
-      <script>
-          const person = {
-              firstName:"John",
-                lastName: "Doe",
-              display: function () {
-    let x = document.getElementById("demo");
-    x.innerHTML = this.firstName + " " + this.lastName;
-          }
-        }
+    <div class="container text-center">
+        <div class="row align-items-start">
+            <div class="col">
+                <!-- Add an element with the ID "demo" for the output -->
+                <p id="demo"></p> <!-- This will hold the output "John Doe" -->
+                <script>
+                    const person = {
+                        firstName: "John",
+                        lastName: "Doe",
+                        display: function () {
+                            let x = document.getElementById("demo");
+                            x.innerHTML = this.firstName + " " + this.lastName;
+                        }
+                    };
 
-        let display = person.display.bind(person);
-        setTimeout(display, 3000);
-      </script>
+                    // Bind the display method and call it after 3 seconds
+                    let display = person.display.bind(person);
+                    setTimeout(display, 3000);
+                </script>
+            </div>
+            <div class="col">
+                One of three columns
+            </div>
+            <div class="col">
+                One of three columns
+            </div>
+        </div>
     </div>
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-  </div>
-</div>
 </body>
 </html>
