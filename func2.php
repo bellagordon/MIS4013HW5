0318fed7b8fcaad26ecb5377f01852f3bd7fa063
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-04 19:38:09 -0600

    Update func2.php

--- func2.php
+++ func2.php
@@ -8,7 +8,19 @@
 <div class="container text-center">
   <div class="row align-items-start">
     <div class="col">
-      One of three columns
+      <script>
+          const person = {
+              firstName:"John",
+                lastName: "Doe",
+              display: function () {
+    let x = document.getElementById("demo");
+    x.innerHTML = this.firstName + " " + this.lastName;
+          }
+        }
+
+        let display = person.display.bind(person);
+        setTimeout(display, 3000);
+      </script>
     </div>
     <div class="col">
       One of three columns
