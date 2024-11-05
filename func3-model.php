a98c3ee72e1979baf3ac5c978a9d4f36c0c0d69e
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-05 17:22:43 -0600

    Update func3-model.php

--- func3-model.php
+++ func3-model.php
@@ -7,12 +7,13 @@
   <div class="col">
     <div class="collapse multi-collapse" id="multiCollapseExample1">
       <div class="card card-body">
+        <p id="myEquation"></p>
         <script>
-      function myFunction(a, b, c) {
+      function myEquation(a, b, c) {
       return (a * b) * c;
       }
 
-      let x = myFunction(12, 15, 3);     
+      let x = myEquation(12, 15, 3);     
       </script>
       </div>
     </div>
