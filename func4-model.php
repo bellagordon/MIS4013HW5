82fa9c07b722ce04832e8a10f6f73d02696107a1
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-05 17:55:36 -0600

    Update func4-model.php

--- func4-model.php
+++ func4-model.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-  
+  <h1>Function 4</h1>
 </head>
   
 <body>
+  <script>
+    const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(random(1, 100000));
+  </script>
   
 </body>
   
