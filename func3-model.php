0c35d62fb241fe4489e4515150c6cb4043f4fe74
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-05 17:47:33 -0600

    Update func3-model.php

--- func3-model.php
+++ func3-model.php
@@ -39,8 +39,9 @@
       }
 
       let x = myEquation(12, 15, 3); 
-      document.getElementById("x").innerText = x; // Display the result inside the <p> element
+      document.getElementById("x").innerText = x; 
       </script>      
+          
       </div>
     </div>
   </div>
