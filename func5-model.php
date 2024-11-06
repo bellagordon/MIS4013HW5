a0c1fcd1311d0d82283253af0ff43f2866ada79c
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-05 18:53:31 -0600

    Create func5-model.php

---
+++ func5-model.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en"> 
+
+<head>
+  <title>Function 5t</title>
+</head> 
+
+<body>
+  <div id="output"></div> <!-- Placeholder for the function output -->
+
+  <script>
+    function foo(i) {
+      if (i < 0) {
+        return;
+      }
+      document.getElementById("output").innerHTML += `begin: ${i}<br>`;
+      foo(i - 1);
+      document.getElementById("output").innerHTML += `end: ${i}<br>`;
+    }
+    foo(10);
+  </script>
+</body>
+  
+</html>
