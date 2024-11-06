2ebd7c3085d33683f70e49cd8e59048377f178dd
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-05 18:16:55 -0600

    Update func4-model.php

--- func4-model.php
+++ func4-model.php
@@ -7,32 +7,32 @@
 <body>
   <ul class="list-group">
   <li class="list-group-item"> Random number 1 : 
-  <p id="randomNum"></p> <!-- Placeholder for the random number -->
+  <p id="randomNum1"></p> <!-- Placeholder for the random number -->
   
   <script> 
     const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
     const result = randomNum(1, 100000);  // Generate random number
-    document.getElementById("randomNum").textContent = result;  // Display result in the <p> tag
+    document.getElementById("randomNum1").textContent = result;  // Display result in the <p> tag
  </script>
     
   </li>
   <li class="list-group-item"> Random number 2 : 
-  <p id="randomNum"></p> <!-- Placeholder for the random number -->
+  <p id="randomNum2"></p> <!-- Placeholder for the random number -->
   
   <script> 
     const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
     const result = randomNum(1, 100000);  // Generate random number
-    document.getElementById("randomNum").textContent = result;  // Display result in the <p> tag
+    document.getElementById("randomNum2").textContent = result;  // Display result in the <p> tag
  </script>
     
   </li>
   <li class="list-group-item"> Random number 3 : 
-  <p id="randomNum"></p> <!-- Placeholder for the random number -->
+  <p id="randomNum3"></p> <!-- Placeholder for the random number -->
   
   <script> 
     const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
     const result = randomNum(1, 100000);  // Generate random number
-    document.getElementById("randomNum").textContent = result;  // Display result in the <p> tag
+    document.getElementById("randomNum3").textContent = result;  // Display result in the <p> tag
  </script>
     
   </li>
