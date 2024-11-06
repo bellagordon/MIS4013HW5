d7bd0468ec6f45b6d5ccc159d243b3a3809b4d89
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-05 18:07:03 -0600

    Update func4-model.php

--- func4-model.php
+++ func4-model.php
@@ -7,12 +7,11 @@
 <body>
   <p id="randomNum"></p> <!-- Placeholder for the random number -->
   
-  <script>
+  <script> 
     const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
     const result = randomNum(1, 100000);  // Generate random number
     document.getElementById("randomNum").textContent = result;  // Display result in the <p> tag
-  </script>
+ </script>
   
-</body>
-  
+</body>  
 </html>
